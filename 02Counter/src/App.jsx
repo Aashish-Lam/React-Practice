a132334092e61b5ee0d5a841f5e7cf4cc45d0648
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-04-18 17:48:17 +0545

    counter using useState()

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [Counter,setCounter]=useState(0)//propogates to user interface
+  // let Counter=15
+  const addValue= ()=>{
+    Counter=Counter+1;//mathi ko counter varr 0 ma initialize vayesi addition gareko
+    setCounter(Counter)//update counter haru tyo mathiko method ma pass gareko
+  }
+  //similary decrease the value
+  const removeValue= ()=>{
+    Counter=Counter-1;
+    setCounter(Counter)
+  }
+  return (
+    <>
+     <div>
+      <h1>Chai aur react</h1>
+      <h2>Counter value : {Counter}</h2>
+      <button onClick={addValue}>Add value{Counter}</button>
+      <br/>
+      <br/>
+      <button
+      onClick={removeValue}>Remove Value{Counter}</button>
+     </div>
+    </>
+  )
+}
+
+export default App
